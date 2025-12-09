be8412db4bb63625f63c616df69784e40a064c58
maxwell-gao-mac24 <maxwell-gao314@outlook.com>
2025-12-09 15:51:21 +0800

    Add dis_schedule parameter to LoopTransformer configuration

--- models/recursive_reasoning/loop_transformer.py
+++ models/recursive_reasoning/loop_transformer.py
@@ -79,6 +79,7 @@ class LoopTransformerConfig(BaseModel):
 
     dis_enabled: bool = False
     dis_max_steps: int = 16
+    dis_schedule: str = "linear"
 
     outer_cycles: int
     warmup_cycles: Optional[int] = None
